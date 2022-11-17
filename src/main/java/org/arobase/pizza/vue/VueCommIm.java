dbfea73ad3e0df4ef6c0619e647b18e35d025f35
POVOAS Florian <florian_povoas@icloud.com>
2022-11-17 08:44:31 +0100

    Mofication des vues

--- src/main/java/org/arobase/pizza/vue/VueCommIm.java
+++ src/main/java/org/arobase/pizza/vue/VueCommIm.java
@@ -5,6 +5,7 @@ import javafx.geometry.Pos;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import org.arobase.pizza.MyImage;
 import org.arobase.pizza.model.ModeleCommande;
@@ -23,20 +24,21 @@ public class VueCommIm extends GridPane implements Observateur {
 		this.setPadding(new Insets(15));
 		this.setHgap(25);
 		this.setAlignment(Pos.CENTER);
-		Image mim= new Image("file:src/main/resources/org/arobase/pizza/fond_creme.png",true);
-		Image ing= new Image("file:src/main/resources/org/arobase/pizza/ing_fromage.png",true);
-		for(int i=0 ; i<4 ; i++){//Affiche 4 fois la meme pizza de base dans le JPanel courant
-			MyImage myIm= new MyImage("file:src/main/resources/org/arobase/pizza/fond_creme.png", null);
-			//myIm.superposer("/ing_fromage.png"); //Permet de superposer un ingrédient
-			this.add(myIm.getSp(),i,1);
-			Rectangle rect= new Rectangle(200,200, Color.rgb(255,255,255,0.2)); // création d'un rectangle entourant l'image
-			rect.setStroke(Color.BLACK);
-			myIm.getSp().getChildren().add(rect);
 		}
-	}
 
 	@Override
 	public void actualiser(Sujet s) {
-		// TODO Auto-generated method stub
+		ModeleCommande m = (ModeleCommande) s;
+		this.getChildren().clear();
+		int pizzaCourante = m.getNumPizzaCourante();
+		for (int i = 0; i < m.getListePizza().size(); i++) {
+			MyImage image = m.getListePizza().get(i).getPizzaIm();
+			this.add(image.getSp(),i,1);
+			if (i == pizzaCourante) {
+				Rectangle rect= new Rectangle(200,200, Color.rgb(255,255,255,0.2)); // création d'un rectangle entourant l'image
+				rect.setStroke(Color.BLACK);
+				image.getSp().getChildren().add(rect);
+			}
+		}
 	}
 }
